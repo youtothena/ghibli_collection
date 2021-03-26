38c4797d160a097d55ba9141b4690bb4f71e4d89
youtothena <80936447+youtothena@users.noreply.github.com>
2021-03-26 16:11:22 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import Films from './Films';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Detail from './Detail';
 
 const App = () => {
   return(
     <>
-      <HashRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
           <Switch>
             <Route path='/' exact={true} component={Films} />
             <Route path='/detail' component={Detail} />
           </Switch>
-      </HashRouter>
+      </BrowserRouter>
     </>
   );
 };
