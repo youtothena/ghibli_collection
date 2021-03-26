e6b75d3b403c7b466994fa6382136b0fb8d3701d
youtothena <parkyouna@bag-yunaui-MacBookPro.local>
2021-03-26 15:40:55 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import Films from './Films';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import Detail from './Detail';
 
 const App = () => {
   return(
     <>
-      <Router>
-        <Switch>
+      <HashRouter>
           <Route path='/' exact={true} component={Films} />
           <Route path='/detail' component={Detail} />
-        </Switch>
-      </Router>
+      </HashRouter>
     </>
   );
 };
--- src/Films.js
+++ src/Films.js
@@ -25,86 +25,86 @@ export default Films;
 const aniImage = [
     {   
         title: 'Castle in the Sky',
-        image: "/images/img01.jpg"
+        image: process.env.PUBLIC_URL+"/images/img01.jpg"
     },
     {
         title: 'Grave of the Fireflies',
-        image: "/images/img02.jpg"
+        image: process.env.PUBLIC_URL+"/images/img02.jpg"
     },
     {
         title: "My Neighbor Totoro",
-        image: "/images/img03.jpg"
+        image: process.env.PUBLIC_URL+"/images/img03.jpg"
     },
     {
         title: "Kiki's Delivery Service",
-        image: "/images/img04.jpg"
+        image: process.env.PUBLIC_URL+"/images/img04.jpg"
     },
     {
         title: "Only Yesterday",
-        image: "/images/img05.jpg"
+        image: process.env.PUBLIC_URL+"/images/img05.jpg"
     },
     {
         title: "Porco Rosso",
-        image: "/images/img06.jpg"
+        image: process.env.PUBLIC_URL+"/images/img06.jpg"
     },
     {
         title: "Pom Poko",
-        image: "/images/img07.jpg"
+        image: process.env.PUBLIC_URL+"/images/img07.jpg"
     },
     {
         title: "Whisper of the Heart",
-        image: "/images/img08.jpg"
+        image: process.env.PUBLIC_URL+"/images/img08.jpg"
     },
     {
         title: "Princess Mononoke",
-        image: "/images/img09.jpg"
+        image: process.env.PUBLIC_URL+"/images/img09.jpg"
     },
     {
         title: "My Neighbors the Yamadas",
-        image: "/images/img10.jpg"
+        image: process.env.PUBLIC_URL+"/images/img10.jpg"
     },
     {
         title: "Spirited Away",
-        image: "/images/img11.jpg"
+        image: process.env.PUBLIC_URL+"/images/img11.jpg"
     },
     {
         title: "The Cat Returns",
-        image: "/images/img12.jpg"
+        image: process.env.PUBLIC_URL+"/images/img12.jpg"
     },
     {
         title: "Howl's Moving Castle",
-        image: "/images/img13.jpg"
+        image: process.env.PUBLIC_URL+"/images/img13.jpg"
     },
     {
         title: "Tales from Earthsea",
-        image: "/images/img14.jpg"
+        image: process.env.PUBLIC_URL+"/images/img14.jpg"
     },
     {
         title:"Ponyo",
-        image: "/images/img15.jpg"
+        image: process.env.PUBLIC_URL+"/images/img15.jpg"
     },
     {
         title: "Arrietty",
-        image: "/images/img16.jpg"
+        image: process.env.PUBLIC_URL+"/images/img16.jpg"
     },
     {
         title: "From Up on Poppy Hill",
-        image: "/images/img17.jpg"
+        image: process.env.PUBLIC_URL+"/images/img17.jpg"
     },
     {
         title: "The Wind Rises",
-        image: "/images/img18.jpg"
+        image: process.env.PUBLIC_URL+"/images/img18.jpg"
     },
     {
         title: "The Tale of the Princess Kaguya",
-        image: "/images/img19.jpg"
+        image: process.env.PUBLIC_URL+"/images/img19.jpg"
     },
     {
         title: "When Marnie Was There",
-        image: "/images/img20.jpg"
+        image: process.env.PUBLIC_URL+"/images/img20.jpg"
     },
     {
         title: "The Red Turtle",
-        image: "/images/img21.jpg"
+        image: process.env.PUBLIC_URL+"/images/img21.jpg"
     },
 ]
